d22673da306a45c283f60e7a8676c7e4d51d730e
ajimenezm23 <ajimenezm@edu.tecnocampus.cat>
2021-11-24 16:58:51 +0100

    puente del derecho

---
+++ Assets/forceApplier.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forceApplier : MonoBehaviour
+{
+    [SerializeField] private Rigidbody bridgeRB;
+    [SerializeField] private float bridgeForce; 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.CompareTag("Bridge"))
+            bridgeRB.AddForceAtPosition(-hit.normal * bridgeForce, hit.point);
+    }
+
+}
